7f1d150b5e122411e6eebc016173fce26919253c
danijha-lee <84164688+danijha-lee@users.noreply.github.com>
2021-10-05 15:50:46 -0400

    needs styling

--- js/site.js
+++ js/site.js
@@ -1,31 +1,75 @@
-function displayMessage() {
-    let msg = "";
+function getNumbers() {
+    //access the webpage and get the values from the inputs
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
 
-    msg = document.getElementById("message").value;
+    let newStart = parseInt(startValue);
+    let newEnd = parseInt(endValue);
 
-    alert(msg);
+    let errorState = false;
+    let errorMsg = "";
+
+    if (isNaN(newStart) || isNaN(newEnd)) {
+        errorState = true;
+        errorMsg += "Please use numbers<hr/>";
+
+    }
+
+    if (newStart > newEnd) {
+        errorState = true;
+        errorMsg += "Start value must be less than end value<hr/>";
+    }
+
+    if (newStart > 10000 || newStart < -10000 || newEnd > 10000 || newEnd < -10000) {
+        errorState = true;
+        errorMsg += "-10,0000 and 10,000 are the limit<hr/>"
+    }
+
+    if (errorState) {
+        Swal.fire(
+            'Something went wrong',
+            `${errorMsg}`,
+            'error'
+        )
+        return;
+    }
+    //take the start and end values as the bounds for a 'for' loop
+    //that will create an array and return it
+    let numbers = generateNumbers(newStart, newEnd);
+
+    //take the array that was returned and pass it to a function that will
+    //display it
+    displayNumbers(numbers);
 }
 
+//Wrapper function/method - a function that calls other functions
 
-function displayMessage2() {
-    let msg = "";
+function generateNumbers(startValue, endValue) {
+    let numbers = [];
 
-    msg = document.getElementById("message").value;
+    //loop over every number from startValue to endValue
+    for (let index = startValue; index <= endValue; index++) {
+        numbers.push(index);
+    }
 
-    Swal.fire(msg);
+    return numbers;
 }
 
+function displayNumbers(fluffyKitty) {
+    let className = "even";
+    let templateRows = "";
 
-// Two functions
-// function displayMessage(msg) {
+    for (let index = 0; index < fluffyKitty.length; index++) {
+        let number = fluffyKitty[index];
 
-//     alert(msg);
-// }
+        if (number % 2 == 0) {
+            className = "even";
+        } else {
+            className = "odd";
+        }
 
-// function getMessage() {
-//     let msg = "";
+        templateRows = templateRows + `<tr><td class="${className}">${number}</td></tr>`;
+    }
 
-//     msg = document.getElementById("message").value;
-
-//     displayMessage(msg);
-// }
+    document.getElementById("output").innerHTML = templateRows;
+}
